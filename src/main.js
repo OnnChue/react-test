da7a27d7d13e9fcb81b7797d461f0b968a8dbee9
OnnChue <onnchue2@gmail.com>
2025-06-03 02:31:58 +0800

    react-day1-finish

--- src/main.js
+++ src/main.js
@@ -1,7 +1,27 @@
+import { createElement } from "react";
 import "./index.css";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-const categories = ["electronics", "jewelery", "men's clothing", "women's clothing"]
+// const categories = [
+//   "electronics",
+//   "jewelery",
+//   "men's clothing",
+//   "women's clothing",
+// ];
 
+const root = document.querySelector("#root");
+
+//view
+// const app = createElement(
+//     "div",
+//     { id: "category-section", className: "p-5" },
+//     createElement("p", { className: "text-2xl text-gray-500 mb-3" }, null, "Product Categories"),
+//     createElement("div", null, ...categories.map((category) => createElement("button", { className: "border border-black px-4 py-2 me-2" }, null, category)))
+// );
+
+//render
+createRoot(root).render(App());
 
 // const root = document.querySelector("#root");
 
@@ -17,7 +37,4 @@ const categories = ["electronics", "jewelery", "men's clothing", "women's clothi
 // };
 // root.append(title);
 
-
 // categories.forEach((category) => root.append(categoryButton(category)));
-
-
