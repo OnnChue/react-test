459b5b49a936c81cd877201c442ac5e865deec72
OnnChue <onnchue2@gmail.com>
2025-06-03 01:43:45 +0800

    second

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import "./index.css";
+
+const categories = ["electronics", "jewelery", "men's clothing", "women's clothing"]
+
+
+// const root = document.querySelector("#root");
+
+// const title = document.createElement("p");
+// title.innerText = "Product Categories";
+// title.classList.add("text-2xl", "text-gray-500", "mb-3");
+
+// const categoryButton = (categoryName) => {
+//     const btn = document.createElement("button");
+//     btn.innerText = categoryName;
+//     btn.classList.add("border", "border-black",  "px-4", "py-2", "me-2", )
+//     return btn;
+// };
+// root.append(title);
+
+
+// categories.forEach((category) => root.append(categoryButton(category)));
+
+
